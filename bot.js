ce9e50df2bf3f4912c25946a71a8243505e0bf7f
Ziyad-Aljunaidi <ziyad.al.junaidi@gmail.com>
2021-08-27 08:23:55 +0200

    removed attachment

--- bot.js
+++ bot.js
@@ -119,31 +119,7 @@ function handleMessage(sender_psid, received_message) {
     else if (received_message.attachments) {
         // Gets the URL of the message attachment
         let attachment_url = received_message.attachments[0].payload.url;
-        response = {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": [{
-                        "title": "Is this the right picture?",
-                        "subtitle": "Tap a button to answer.",
-                        "image_url": attachment_url,
-                        "buttons": [
-                            {
-                                "type": "postback",
-                                "title": "Yes!",
-                                "payload": "yes",
-                            },
-                            {
-                                "type": "postback",
-                                "title": "No!",
-                                "payload": "no",
-                            }
-                        ],
-                    }]
-                }
-            }
-        }
+        response = {"text": ""}
     }
     else if(received_message.text) {
         response = config.quick_err_handling;
