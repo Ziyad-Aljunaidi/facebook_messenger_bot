c0b62f506ea9642088ab11efefea593d50a774f4
Ziyad-Aljunaidi <ziyad.al.junaidi@gmail.com>
2021-08-30 09:27:45 +0200

    jst push it

--- bot.js
+++ bot.js
@@ -176,7 +176,7 @@ function handleMessage(sender_psid, received_message) {
     }
 
     // Sends the response message
-    callSendAPI(sender_psid, response);
+    //callSendAPI(sender_psid, response);
 }
 
 // Handles messaging_postback events
